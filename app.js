780c3fa36198180da96bd33895e533e874715d16
Fauzan <mukhlizardyal@gmail.com>
2024-07-17 13:57:24 +0700

    mengubah codingan app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true })); // Middleware untuk mendukung x-www-form-urlencoded
 app.use(upload.array());
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5100;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
@@ -126,7 +126,7 @@ app.delete("/purchases/:id_purchase", (req, res) => {
   );
 });
 
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
-  console.log(`Electronics store service running on port ${PORT}`);
-});
+//const PORT = process.env.PORT || 3001;
+//app.listen(PORT, () => {
+//  console.log(`Electronics store service running on port ${PORT}`);
+//});
