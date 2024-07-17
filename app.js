85aa154087e43ad06f81acbdee3f267f816d088b
Fauzan <mukhlizardyal@gmail.com>
2024-07-17 11:45:47 +0700

    mengubah codingan app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true })); // Middleware untuk mendukung x-www-form-urlencoded
 app.use(upload.array());
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5100;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
